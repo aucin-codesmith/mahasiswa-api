1e9b12e19466db236a27e4435385ded93daabf06
aucin-codesmith <muhammadyusuf8089@gmail.com>
2026-08-10 22:41:01 +0700

    fix: perbaiki koneksi database dan schema yang kosong

--- db/schema.js
+++ db/schema.js
@@ -1,4 +1,4 @@
-﻿const { pgTable, serial, varchar, integer } = require('drizzle-orm/pg-core');
+const { pgTable, serial, varchar, integer } = require('drizzle-orm/pg-core');
 
 const mahasiswa = pgTable('mahasiswa', {
   id: serial('id').primaryKey(),
